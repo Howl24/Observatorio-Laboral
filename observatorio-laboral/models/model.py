69fb079b9d7fc843f4681f48138c97a13bc37e8b
Daniel Matos <daniel.matos@pucp.pe>
2017-11-02 19:57:06 -0500

    Add classmethod decorator to abstractmethods

--- observatorio-laboral/models/model.py
+++ observatorio-laboral/models/model.py
@@ -7,16 +7,35 @@ import abc
 
 
 class CassandraModel(object, metaclass=abc.ABCMeta):
-
     """
     Class that contains all the common functions for cassandra conection.
     """
 
+    @classmethod
     @abc.abstractmethod
     def CreateTables(cls):
         raise NotImplementedError('Users must define CreateTables to use this class')
     
 
+    @classmethod
     @abc.abstractmethod
     def ConnectToDatabase(cls):
         raise NotImplementedError('Users must define ConnectToDatabase to use this class')
+
+
+#"""Class Example
+
+#class Foo(CassandraModel):
+#
+#    def __init__(self):
+#        pass
+#
+#    @classmethod
+#    def CreateTables(cls):
+#        pass
+#
+#    @classmethod
+#    def ConnectToDatabase(cls):
+#        pass
+
+#"""

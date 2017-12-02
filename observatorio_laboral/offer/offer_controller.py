db26e58b0231cce63c892d4074c561b6a14be728
Daniel Matos <daniel.matos@pucp.pe>
2017-12-02 09:18:44 -0500

    Add return list to get offer and tests

--- observatorio_laboral/offer/offer_controller.py
+++ observatorio_laboral/offer/offer_controller.py
@@ -13,38 +13,18 @@ class OfferController(object):
     def get_offers(self, source=None, date_range=None):
         """
             date_range -> Tuple (min_date, max_date)
-            min_date   -> Tuple (month, year)
-            max_date   -> Tuple (month, year)
         """
 
+        selected_offers = []
         for date in date_range:
-            query_params = (source, date.get_month(), date.get_year())
+            query_params = (source, date.get_year(), date.get_month())
             offers = Offer.Query('select', query_params)
-            print(date)
+            selected_offers += offers
 
+        return selected_offers
 
-# INSERTION TEST
-from cassandra.cluster import Cluster
-cluster = Cluster()
-session = cluster.connect()
-
-rows = session.execute("select * from symplicity.new_offers")
-
-Offer.ConnectToDatabase('l4', 'l4_offers')
-
+date_range = DateRange(4, 2016, 6, 2016)
 source = "symplicity"
-for row in rows:
-    year = row.year
-    month = row.month
-    id = row.id
-    features = row.features
-    careers = row.careers
 
-    offer = Offer(source, year, month, id, features, careers)
-    offer.Insert()
-
-#date_range = DateRange(6, 2016, 3, 2017)
-#source = "symplicity"
-#
-#offer_controller = OfferController()
-#offer_controller.get_offers(source, date_range)
+offer_controller = OfferController()
+offer_controller.get_offers(source, date_range)
--- observatorio_laboral/offer/test.py
+++ observatorio_laboral/offer/test.py
@@ -1,12 +1,13 @@
 from cassandra.cluster import Cluster
-from offer import OfferModel
+from offer import Offer
 
+# INSERTION TEST
 cluster = Cluster()
 session = cluster.connect()
 
 rows = session.execute("select * from symplicity.new_offers")
 
-OfferModel.ConnectToDatabase('l4', 'l4_offers')
+Offer.ConnectToDatabase('l4', 'l4_offers')
 
 source = "symplicity"
 for row in rows:
@@ -16,5 +17,15 @@ for row in rows:
     features = row.features
     careers = row.careers
 
-    offer = OfferModel(source, year, month, id, features, careers)
+    offer = Offer(source, year, month, id, features, careers)
     offer.Insert()
+
+# SELECT TESTS
+# BY SOURCE AND DATE RANGE
+date_range = DateRange(4, 2016, 6, 2016)
+source = "symplicity"
+
+offer_controller = OfferController()
+offer_controller.get_offers(source, date_range)
+
+

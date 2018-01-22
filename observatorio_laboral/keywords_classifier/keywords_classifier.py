f4da1dbb3f1e8c80ccbfd1df38bc519cebe86173
Daniel Matos <daniel.matos@pucp.pe>
2018-01-22 08:21:09 -0500

    Add keyword classifier

--- observatorio_laboral/keywords_classifier/keywords_classifier.py
+++ observatorio_laboral/keywords_classifier/keywords_classifier.py
@@ -1,4 +1,6 @@
 from .keyword import KeyWord
+from nltk.stem.snowball import SpanishStemmer
+from nltk.corpus import stopwords
 
 def readNumber(file, error=None):
     try:
@@ -51,3 +53,63 @@ class KeyWordClassifier():
             query_params = (category,)
             print(query_params)
             self.keywords[category] = KeyWord.Query('select', query_params)
+
+
+    def stem_text(self, text):
+        newText = ""
+        for word in text.split():
+            wordStemmed = SpanishStemmer.stem(word)
+            newText += wordStemmed + ' '
+
+        return newText.strip()
+
+
+    def process(self, category, proc_code, features):
+        tok = CountVectorizer(stop_words=stopwords.words("spanish")).build_tokenizer()
+
+        feats = []
+        for feature in features:
+            tokens = tok(feature)
+            feats.append(tokens)
+
+        data = " ".join(feats)
+        data_stem = self.stem_text(data)
+        found = set()
+        if proc_code == FIND_WORDS_PROC_CODE:
+            for kw in self.keywords[category]:
+                for search_stem in kw.similars_stem:
+                    if search_stem in data_stem.split():
+                        found.add(kw.word)
+
+                for search in kw.similars_no_stem:
+                    if search in data.split():
+                        found.add(kw.word)
+
+        if proc_code == FIND_PRIORITY_PHRASE_PROC_CODE:
+            for kw in self.keywords[category]:
+                for search_stem in kw.similars_stem:
+                    if search_stem in data_stem.split():
+                        found.add(kw.word)
+
+                for seach in kw.similars_no_stem:
+                    if seach in data.split():
+                        found.add(kw.word)
+
+                # Return the first match
+                if found:
+                    return found
+
+        return found
+
+    def run(self, data):
+        results = []
+        for offer in data:
+            of_res = {}
+            for category, proc_code, features in configurations:
+                feats = [feat for feat in offer if feat in features]
+
+                of_res[category] = self.process(category, proc_code, feats)
+
+            results.append(of_res)
+
+        return results

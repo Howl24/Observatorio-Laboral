b083e0f3c8f6f8052863f2aa31074c82bb8ff557
Daniel Matos <daniel.matos@pucp.pe>
2018-02-14 13:11:19 -0500

    import test

--- test_offer.py
+++ test_offer.py
@@ -4,6 +4,8 @@ from cassandra.cluster import NoHostAvailable
 #from observatorio_laboral.offer.offer_controller import OfferController
 #from observatorio_laboral.offer.date_range import DateRange
 from observatorio_laboral.offer.offer import Offer
+from observatorio_laboral.offer import OfferController
+from observatorio_laboral.offer import DateRange
 
 logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
 
@@ -46,10 +48,32 @@ def load_offers():
     Offer.Import(keyspace, all_offers_table,
                  "Backup/Data/data_symplicity_new_offers")
 
+def test_export():
+    oc = OfferController('l4_test', '')
+
+
+    Offer.Export(keyspace, all_offers_table,
+                 "Backup/Data/Export/Last.csv", offer_list)
+
+def test_import():
+    keyspace = 'l4_test'
+    table = 'all_offers'
+    Offer.ConnectToDatabase(keyspace, table)
+    Offer.Import(keyspace, table,
+                 filename='/home/howl24/Data/test_data_aptitus_new_offers.csv')
+
+#    oc = OfferController(keyspace='l4_test',
+#                         table='all_offers')
+#
+#    oc.from_csv("/home/howl24/Data/test_data_aptitus_new_offers")
+#
+                    
 if __name__ == "__main__":
+    test_import()
     #setup()
-    prepare_statements()
-    load_offers()
+    #prepare_statements()
+
+    #load_offers()
 
 #date_range = DateRange(1, 2013, 5, 2017)
 #source = "symplicity"
